8c7b3bf73e901de1f8ffe1dad543158aec15d047
gaoqiang <gaoqiang@yundun119.com>
2023-01-11 10:59:01 +0800

    位置修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,17 +2,10 @@ module.exports = {
   title: '我的文档地址',
   description: '在线文档地址',
   themeConfig: {
-    sidebarDepth:2,
+    sidebarDepth: 2,
     smoothScroll: true,
     nav: [
       {text: '主页', link: '/'},
-      {
-        text: '常用配置',
-        items: [
-          {text: 'vite常用配置', link: '/config/vite-config'},
-          {text: 'tailwindcss的配置', link: '/config/tailwindcss'},
-        ],
-      },
       {
         text: '基础知识',
         items: [
@@ -20,11 +13,18 @@ module.exports = {
           {text: '数组', link: '/base/arr'},
           {text: '对象', link: '/base/object'},
         ]
+      },
+      {
+        text: '常用配置',
+        items: [
+          {text: 'vite常用配置', link: '/config/vite-config'},
+          {text: 'tailwindcss的配置', link: '/config/tailwindcss'},
+        ],
       }
     ],
     sidebar: {
       '/config/': ['vite-config', 'tailwindcss'],
-      '/base/': ['reg','arr','object'],
+      '/base/': ['reg', 'arr', 'object'],
     }
   },
 }
