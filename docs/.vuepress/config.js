a9623845e2fbe6a36ac61395214f66c7aae81559
gaoqiang <gaoqiang@yundun119.com>
2023-11-03 10:21:18 +0800

    [合并分支] [from dev] [begin:f09e6ccf3d0df1b6743d982f8810962cfdef30f4, end:7c7b517f887c609334ba80434bbe90dc51f31eb1]

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,10 @@ const base = require('./base');
 const sidebar = require("./sidebar")
 
 module.exports = {
+  plugins:["@vuepress/back-to-top"],
   title: '我的文档地址',
   description: '在线文档地址',
+  base: '/vue-preess/',
   themeConfig: {
     sidebarDepth: 2,
     smoothScroll: true,
@@ -20,13 +22,6 @@ module.exports = {
           {text: 'electron', link: '/cross/electron'},
         ]
       },
-      {
-        text: '题库',
-        items: [
-          {text: '高难度', link: '/question/height'},
-          {text: '中难度', link: '/question/middle'},
-        ]
-      },
       {text: '常用插件', link: '/plugin/plugin'},
       {
         text: '常用配置',
@@ -34,7 +29,8 @@ module.exports = {
           {text: 'vite常用配置', link: '/config/vite-config'},
           {text: 'tailwindcss的配置', link: '/config/tailwindcss'},
           {text: 'nodemon配置', link: '/config/nodemon'},
-          {text: 'git', link: '/config/git'},
+          {text: 'git', link: '/config/git' },
+          {text:'vscode',link:'/config/vscode'}
         ],
       },
       {
--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '/config/': ['vite-config', 'tailwindcss','nodemon','git'],
+  '/config/': ['vite-config', 'tailwindcss','nodemon','git','vscode'],
   '/base/': ['reg', 'arr', 'object','str','window'],
   '/plugin/': ['plugin'],
   '/question/': ['height','middle'],
